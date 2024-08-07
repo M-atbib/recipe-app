e782a88fd11492930802a87278c46f53a596ccc4
unknown <mokhtaratbib567@gmail.com>
2024-08-07 19:22:30 +0100

    testing

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,6 +12,10 @@ import {
   Typography,
   Link,
 } from "@mui/material";
+import {
+  Facebook as FacebookIcon,
+  Google as GoogleIcon,
+} from "@mui/icons-material";
 import axios from "axios";
 
 const Login = () => {
@@ -19,17 +23,9 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [keepSignedIn, setKeepSignedIn] = useState(true);
 
-  const getCsrfToken = () => {
-    const cookies = document.cookie
-      .split("; ")
-      .find((row) => row.startsWith("XSRF-TOKEN"));
-    return cookies ? cookies.split("=")[1] : null;
-  };
-
   const handleLogin = async (e: FormEvent) => {
     e.preventDefault();
     try {
-      // Get CSRF token
       await axios.get(
         "https://gestion-groupeelhouria-d5bfba1b9bb0.herokuapp.com/sanctum/csrf-cookie",
         {
@@ -37,35 +33,17 @@ const Login = () => {
         }
       );
 
-      const csrfToken = getCsrfToken();
-      if (!csrfToken) {
-        throw new Error("CSRF token not found");
-      }
-
-      // Perform login
       const response = await axios.post(
         "https://gestion-groupeelhouria-d5bfba1b9bb0.herokuapp.com/api/auth/login",
         {
           email,
           password,
-        },
-        {
-          withCredentials: true,
-          headers: {
-            "X-XSRF-TOKEN": csrfToken,
-          },
         }
       );
 
       console.log(response.data);
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        // Axios specific error handling
-        console.error("Axios error:", error.response?.data || error.message);
-      } else {
-        // General error handling
-        console.error("Login failed:", error);
-      }
+      console.error("Login failed:", error);
     }
   };
 
