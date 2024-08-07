16fc81d5d5899608d4d4060693fc6976a8494073
unknown <mokhtaratbib567@gmail.com>
2024-08-07 19:00:38 +0100

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,16 @@
 "use client";
-import { useRouter } from "next/router";
+import Link from "next/link";
 import { Button } from "@mui/material";
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleNavigate = () => {
-    router.push("/login");
-  };
-
   return (
     <main>
       <h1>Hello lhaj lmokhtar</h1>
-      <Button variant="contained" color="primary" onClick={handleNavigate}>
-        Go to Login
-      </Button>
+      <Link href="/login" passHref>
+        <Button variant="contained" color="primary">
+          Go to Login
+        </Button>
+      </Link>
     </main>
   );
 }
